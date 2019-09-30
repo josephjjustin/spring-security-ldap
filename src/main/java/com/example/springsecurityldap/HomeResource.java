f54940c1dc9abecbbcd3f41c52291fdcb16d09fd
Justin Joseph <41144583+josephjjustin@users.noreply.github.com>
2019-09-30 13:06:08 +0400

    first commit

---
+++ src/main/java/com/example/springsecurityldap/HomeResource.java
@@ -0,0 +1,14 @@
+package com.example.springsecurityldap;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResource {
+	
+	@GetMapping("/")
+	public String index() {
+		return  "Home Page";
+	}
+
+}
